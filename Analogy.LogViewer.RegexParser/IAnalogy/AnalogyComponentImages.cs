502d83df1dd57fc6c261adea645c820bcf2d6742
Lior Banai <liorbanai@gmail.com>
2020-07-09 10:07:41 +0300

    add header icon

---
+++ Analogy.LogViewer.RegexParser/IAnalogy/AnalogyComponentImages.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Analogy.DataProviders.Extensions;
+using Analogy.LogViewer.RegexParser.Properties;
+
+namespace Analogy.LogViewer.RegexParser.IAnalogy
+{
+    public class AnalogyComponentImages : IAnalogyComponentImages
+    {
+        public Image GetLargeImage(Guid analogyComponentId)
+        {
+            if (analogyComponentId == PrimaryFactory.Id)
+                return Resources.AnalogyRegex32x32;
+            return null;
+        }
+
+        public Image GetSmallImage(Guid analogyComponentId)
+        {
+            if (analogyComponentId == PrimaryFactory.Id)
+                return Resources.AnalogyRegex16x16;
+            return null;
+        }
+
+        public Image GetOnlineConnectedLargeImage(Guid analogyComponentId) => null;
+
+        public Image GetOnlineConnectedSmallImage(Guid analogyComponentId) => null;
+
+        public Image GetOnlineDisconnectedLargeImage(Guid analogyComponentId) => null;
+
+        public Image GetOnlineDisconnectedSmallImage(Guid analogyComponentId) => null;
+    }
+}
