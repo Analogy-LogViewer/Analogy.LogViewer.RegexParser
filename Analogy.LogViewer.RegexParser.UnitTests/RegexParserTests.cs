0168c8ed96d98559aa367e5d9d55832491bc6e98
Lior Banai <liorb@kama-medical.com>
2020-09-15 15:46:16 +0300

    add unit test

--- Analogy.LogViewer.RegexParser.UnitTests/RegexParserTests.cs
+++ Analogy.LogViewer.RegexParser.UnitTests/RegexParserTests.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Analogy.LogViewer.RegexParser;
+using Analogy.LogViewer.RegexParser.IAnalogy;
 
 namespace Analogy.LogViewer.Serilog.UnitTests
 {
@@ -11,12 +14,19 @@ namespace Analogy.LogViewer.Serilog.UnitTests
         [TestMethod]
         public async Task ClefParserTest()
         {
-            var p = new JsonParser();
+            var p = new RegexOfflineDataProvider();
+            p.SupportFormats=new List<string>(){"*.nlog"};
             CancellationTokenSource cts = new CancellationTokenSource();
-            string fileName = @"testJson.clef";
+            string fileName = @"example.nlog";
             MessageHandlerForTesting forTesting = new MessageHandlerForTesting();
+
+            var regexPattern = new RegexPattern(@"(?<Date>\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{4})\|(?<Level>\w+)\|(?<Source>.+)\|(?<Text>.*)\|(?<ProcessName>.*)\|(?<ProcessId>.*)",
+                "yyyy-MM-dd HH:mm:ss.ffff", "", new List<string> { "*.nlog" });
+            if (!RegexParser.Managers.UserSettingsManager.UserSettings.Settings.RegexPatterns.Contains(regexPattern))
+                RegexParser.Managers.UserSettingsManager.UserSettings.Settings.RegexPatterns.Insert(0, regexPattern);
+
             var messages = await p.Process(fileName, cts.Token, forTesting);
-            Assert.IsTrue(messages.Count() == 2);
+            Assert.IsTrue(messages.Count() == 4);
         }
     }
 }
