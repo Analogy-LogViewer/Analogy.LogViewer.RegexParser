85425571f4f041513a5b8e3e88eca90ed4c103db
Lior Banai <liorbanai@gmail.com>
2022-10-24 19:10:31 +0300

    update nugets

--- Analogy.LogViewer.RegexParser/IAnalogy/RegexUserSettingsFactory.cs
+++ Analogy.LogViewer.RegexParser/IAnalogy/RegexUserSettingsFactory.cs
@@ -8,14 +8,19 @@ using Analogy.LogViewer.Template;
 
 namespace Analogy.LogViewer.RegexParser.IAnalogy
 {
-    public class RegexUserSettingsFactory : UserSettingsFactory
+    public class RegexUserSettingsFactory : TemplateUserSettingsFactory
     {
         public override string Title { get; set; } = "Regex User Settings";
-        public override UserControl DataProviderSettings { get; set; } = new RegexSettingsUC();
+        public override UserControl DataProviderSettings { get; set; }
         public override Image SmallImage { get; set; } = Resources.AnalogyRegex16x16;
         public override Image LargeImage { get; set; } = Resources.AnalogyRegex32x32;
         public override Guid FactoryId { get; set; } = RegexPrimaryFactory.Id;
         public override Guid Id { get; set; } = new Guid("108B0266-E0FB-4A02-B62F-DBB55CA4FEF8");
+
+        public override void CreateUserControl(IAnalogyLogger logger)
+        {
+           DataProviderSettings = new RegexSettingsUC();
+        }
 
         public override Task SaveSettingsAsync()
         {
